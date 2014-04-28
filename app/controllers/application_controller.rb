59c04b33b2e8c84fa1f85629bae4bca3553ce6ed
pjwl33 <paul.leejw@gmail.com>
2014-04-28 10:20:14 -0400

    twitter oauth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  helper_method :signed_in?
+  helper_method :signed_in?, :current_user
 
   private
 
   def signed_in?
     true if session[:oauth_token]
   end
-  
+
+  def current_user
+    if session[:username] != nil
+      return User.find_by(username: session[:username])
+    end
+  end
+
 end
