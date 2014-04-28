33e1a28b74cfe727971c4e6d9de6aa0ecdc48544
jingalli <james@endorse.me>
2014-04-28 16:39:45 -0400

    Got join chatroom feature working.

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,12 +1,26 @@
 class ChatroomsController < ApplicationController
 
+  def index
+    @chatroom = Chatroom.new()
+  end
+
+  def search
+    @chatroom = params[:search]
+    # if params[:search]
+    #   @chatroom = Chatroom.search(params[:search])
+    #   redirect_to chatroom_path(@chatroom)
+    # else
+    #   @chatroom = Chatroom.all.order('created_at DESC')
+    # end
+  end
+
   def create
     @chatroom = Chatroom.create(chatroom_params)
-    redirect_to(@chatroom)
+    redirect_to chatroom_path(@chatroom)
   end
 
   def show
-    @chatroom = Chatroom.find params[:id]
+    @chatroom = Chatroom.find_by(name: params[:search])
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
 
-  root "sessions#index"
+  # root "sessions#index"
+  root "chatrooms#index"
   get '/auth/:provider/callback' => "sessions#create"
   get "/signout" => "sessions#destroy"
+
+  post '/chatrooms' => 'chatrooms#create'
+
+  get '/chatrooms/:name' => 'chatrooms#show'
+
 
 end
