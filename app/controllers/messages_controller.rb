1af918221ff93c49393bce4fb96de00091cb1781
originalblend337 <originalblend337@gmail.com>
2014-05-01 12:23:26 -0400

    fixed hashtag bug

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,9 +4,11 @@ class MessagesController < ApplicationController
 
   def create
     @chatroom = Chatroom.find_by(name: params[:room_name])
-    emotions = %w(happy sad mad surprised rofl confused)
+    emotions = %w(#happy #sad #mad #surprised #rofl #confused)
     emotion = params[:content].scan(/(#\w*)/).flatten.first
-    emotion = 'happy' if emotion.nil? || emotions.include?(emotion) == false
+    if emotion.nil? || emotions.include?(emotion) == false
+      emotion = 'happy'
+    end
     new_content = params[:content].gsub(' ' + emotion, '')
     message = @chatroom.messages.create(content: new_content, chatroom_id: @chatroom.id, user_id: current_user.id, emotion: emotion.gsub('#', ''))
     return_data = {obj: message, user: current_user.username, msg_id: message.id}
